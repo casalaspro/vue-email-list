2ab69e7a3bbe01f56cbfc7dc20e9c1ed8553bfd8
casalaspro <casalaspro.alessandro@gmail.com>
2024-04-08 16:43:18 +0200

    ADD - main files, links, provaApi()

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// console.log("HI VUE!")
+/*
+
+Descrizione:
+Utilizzando Vue
+Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+*/
+
+const { createApp } = Vue;
+
+createApp({
+
+  // DATA
+
+  data(){
+    return{
+      prova: true,
+    }
+  },
+
+  // METHODS
+
+  methods: {
+
+    provaApi(){
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((response=>{
+          console.log(response);
+        }))
+    }
+
+  },
+
+  // CREATED HOOK
+
+  created(){
+    this.provaApi();
+  },
+
+}).mount('#app')
